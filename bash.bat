a70f7d4956070d84122c8c20c6c1e9fc3ff79447
Omkar Kashid <omkarkashid1168@gmail.com>
2026-03-06 10:03:45 +0530

    Create bash.bat

---
+++ bash.bat
@@ -0,0 +1,14 @@
+# Create a virtual environment (recommended)
+python -m venv venv
+
+# Activate the virtual environment
+# On Windows:
+venv\Scripts\activate
+# On macOS/Linux:
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the notebook
+jupyter notebook "Spaceship Titanic.ipynb"
